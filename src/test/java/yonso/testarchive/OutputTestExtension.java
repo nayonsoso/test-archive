d59435cc2e511c94ac709c24e1e0c4d789038cd7
nayonsoso <panda0329@naver.com>
2024-09-28 00:47:31 +0900

    test: 콘솔 출력 검사용 extension 생성

---
+++ src/test/java/yonso/testarchive/OutputTestExtension.java
@@ -0,0 +1,37 @@
+package yonso.testarchive;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.jupiter.api.extension.BeforeEachCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.api.extension.ParameterResolver;
+
+public class OutputTestExtension implements BeforeEachCallback, ParameterResolver {
+
+    PrintStream original;
+    ByteArrayOutputStream outputStream;
+
+    public String getAndPrintOutput() {
+        System.setOut(this.original);
+        System.out.println(this.outputStream.toString());
+        return this.outputStream.toString();
+    }
+
+    @Override
+    public void beforeEach(ExtensionContext context) throws Exception {
+        this.original = System.out;
+        this.outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+    }
+
+    @Override
+    public boolean supportsParameter(ParameterContext parameterContext, ExtensionContext extensionContext) {
+        return parameterContext.getParameter().getType() == OutputTestExtension.class;
+    }
+
+    @Override
+    public Object resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext) {
+        return this;
+    }
+}
