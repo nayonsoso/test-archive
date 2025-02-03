a6efbf2461a407bd3d74e9fded3183bc0628ab80
nayonsoso <panda0329@naver.com>
2025-02-03 12:23:22 +0900

    test: null 을 타입캐스트하면 null이 된다

---
+++ src/test/java/yonso/testarchive/java/typecast/TypeCastingTest.java
@@ -0,0 +1,20 @@
+package yonso.testarchive.java.typecast;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class TypeCastingTest {
+
+    @Test
+    @DisplayName("null 을 타입캐스팅하면 어떻게 되는가?")
+    void test() {
+        assertThatCode(() -> castNull(null)).doesNotThrowAnyException();
+    }
+
+    void castNull(Object obj) {
+        String str = (String) obj;
+        System.out.println(str);
+    }
+}
